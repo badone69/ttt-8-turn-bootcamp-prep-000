70250ad4008199487f67674616eb0762c998a67f
student online <eliel.badon@gmail.com>
2018-09-21 20:59:51 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,41 @@
+
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(spaces)
+  puts " #{spaces[0]} | #{spaces[1]} | #{spaces[2]} "
+  puts "-----------"
+  puts " #{spaces[3]} | #{spaces[4]} | #{spaces[5]} "
+  puts "-----------"
+  puts " #{spaces[6]} | #{spaces[7]} | #{spaces[8]} "
+end
+
+
+def valid_move?(board, index)
+  if position_taken?(board, index)
+    false 
+  elsif index.between?(0, 8)
+    true
+  end 
+end 
+
+def position_taken? (board, index)
+  if board[index] == " " or board[index] == "" or board[index] == nil
+    return false 
+  else
+    return true
+  end
+end
+
+def input_to_index(input)
+  input.to_i-1
+end
+
+def move(board, index, char='X')
+  board[index] = char
+end
+
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.strip
+  user_input = gets.strip 
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board,index)
@@ -11,34 +46,5 @@ def turn(board)
     turn(board)
   end
 end
- def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
- def input_to_index(user_input)
-  user_input = user_input.to_i
-  user_input -= 1
-  return user_input
-end
- def valid_move?(board, index)
-  if index.between?(0,8) && index_check(board, index)
-    TRUE
-  elsif index.between?(0,8) && !index_check(board, index)
-    puts "Selection taken"
-    turn(board)
-  else
-    puts "Please make a valid selection"
-    turn(board)
-  end
-end
- def move(board, index, player = 'X')
-  board[index] = player
-end
- def index_check(board, index)
-  if board[index] == " "
-    TRUE
-  end
-end 
+
+
